6bc4cff5463fe0a6507acd18357db1324c7f0175
unknown <hammad.qadeer@funavry.com-at-89024295807801>
2023-08-18 16:33:28 +0500

    first commit for the javascript chatbot

--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -1,3 +1,37 @@
 let arr = ["john", 23, null, true];
 
 console.log(arr.shift("hello"));
+
+//every()
+
+const every = [1,2,3,4,5,6];
+
+
+//all elements are greater then 5
+const greaterFive = arr.every(num => num > 5);
+console.log(greaterFive);//false
+
+//all elements are less than 9
+const lessnine = arr.every(num => num < 9);
+console.log(lessnine); //true
+
+//some()
+
+const someArr = [1,2,3,4,6,7];
+
+
+// at leaset one element is greater than 5?
+const greaterNum = someArr.some(num => num > 5);
+console.log(greaterNum); //true
+
+const lessNum = someArr.some(num => num <=0);
+console.log(lessNum); //false
+
+//includes()
+
+const includesArr = [1,2,4,5,6,7];
+console.log(includesArr.includes(0)); //false 
+console.log(includesArr.includes(4)); //true
+
+
+
--- JS-Concepts/script.js
+++ JS-Concepts/script.js
@@ -18,3 +18,30 @@ console.log(longWords);
 const total = [0, 1, 2, 4].reduce((sum, value) => sum + value, 1);
 
 console.log(total);
+
+
+const arr = [1, 2, 3, 4, 5,6];
+
+//sort in descending order
+const alphabet = ["f", "a", "r", "t", "e"];
+
+descend = arr.sort((a, b) => a > b ? -1 : 1);
+
+console.log(descend);
+
+//sort in ascending order
+
+ascend = alphabet.sort((a, b) => a > b ? 1 : -1);
+console.log(ascend);
+
+
+const arr1 = [1,2,4];
+
+arr1.forEach(element =>  {
+    console.log(element);
+})
+
+//concat()
+const arr2 = ["a", "b", "c"];
+const arr3 = ["d", "e", "f"];
+
